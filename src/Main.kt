b060202877dc57cbf3bbecfe25c13c5635e3dfde
zyxmn <zyxmndaleyjes@gmail.com>
2024-04-05 16:17:21 +0100

    yolo

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+import kotlin.io.println as println
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+
+    println("Hello, $name!")
+
+    for (i in 1..5) {
+
+        println(message = "i = $i")
+    }
+}
